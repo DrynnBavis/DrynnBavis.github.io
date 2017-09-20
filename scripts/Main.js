b2596c25cb9e187c2323468e5fc23df9891d85f1
drynnbavis <drynnbavis@gmail.com>
2017-09-19 23:43:53 -0400

    fixed an anchor-JS issue preventing hyperlinks from working.

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,9 +1,11 @@
 ﻿
 
 $(document).on('click', 'a', function(event){
-    event.preventDefault();
+    if (!($(event.target).hasClass('footer--socialMedia-item'))){
+        event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+    };
 });
