dfa39fcbea99900762c36b367d098d10d9c66b93
drynnbavis <drynnbavis@gmail.com>
2019-01-10 17:19:03 -0500

    Kodama heads now rattle on document click

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
-// Declare head vars here for onload()
+// Global Kodama head co-ordinates
 var head1 = [0,0];
 var head2 = [0,0];
 var head3 = [0,0];
-
 
 (function() {
     document.onmousemove = handleMouseMove;
@@ -18,7 +17,6 @@ var head3 = [0,0];
             eventDoc = (event.target && event.target.ownerDocument) || document;
             doc = eventDoc.documentElement;
             body = eventDoc.body;
-
             event.pageX = event.clientX +
               (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
               (doc && doc.clientLeft || body && body.clientLeft || 0);
@@ -27,7 +25,7 @@ var head3 = [0,0];
               (doc && doc.clientTop  || body && body.clientTop  || 0 );
         }
 
-        // TODO: Use x/y values to rotate heads
+        // Rotate Kodama heads to follow cursor
         angle1 = Math.atan2( event.pageY - head1[0], event.pageX - head1[1]);
         angle2 = Math.atan2( event.pageY - head2[0], event.pageX - head2[1]);
         angle3 = Math.atan2( event.pageY - head3[0], event.pageX - head3[1]);
@@ -37,11 +35,40 @@ var head3 = [0,0];
     }
 })();
 
+// Rattle their heads when clicked
+$("body").click(function(){
+    // Only rattle if visible
+    if ($(".kodama-container").hasClass("hidden") != true){
+        // Pick a random Kodama
+        var KodamaHead;
+        var random = Math.random() * 3;
+        if (random > 2){
+            KodamaHead = $("#wrapper-head3");        
+        }
+        else if (random > 1){
+            KodamaHead = $("#wrapper-head2");
+        }
+        else{
+            KodamaHead = $("#wrapper-head1");
+        }
+        
+        // Add animation
+        KodamaHead.addClass("rattling");
+        
+        // Remove animation on completion
+        KodamaHead.one("webkitAnimationEnd oanimationend oAnimationEnd msAnimationEnd animationend",   
+        function(e) {
+            $(this).removeClass("rattling");
+        });
+    }
+});
+
+// Toggle Kodama visibility with face click
 $("#face").click(function () {
     // Reveal/hide heads
-    $(".kodama-container").toggleClass("hidden");
+    $(".kodama-container").toggleClass("hidden"); 
     
-    //get location of heads
+    // Get location of heads
     head1 = [$("#kodama-head1").offset().top + $("#kodama-head1").height() / 2, $("#kodama-head1").offset().left + $("#kodama-head1").width() / 2];
     head2 = [$("#kodama-head2").offset().top + $("#kodama-head2").height() / 2, $("#kodama-head2").offset().left + $("#kodama-head2").width() / 2];
     head3 = [$("#kodama-head3").offset().top + $("#kodama-head3").height() / 2, $("#kodama-head3").offset().left + $("#kodama-head3").width() / 2];
